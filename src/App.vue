e0c1a86a9754b208c18b59c8e4e5c173362181d0
Arun R <arun@ftltech.io>
2024-04-28 16:27:21 +0530

    Fix: Show map

--- src/App.vue
+++ src/App.vue
@@ -22,16 +22,54 @@ export default {
       const modelId = 'ca1fc12d33f635cda586e025b27ee0d571b1a3ce42e98e779afe84e966b6ee2c';
       const coordinateSystem = 'ForceZeroOrigin';
 
+      // This position we might need to get from the IFC - unclear
+      const position = [24.91781, 60.15555, -90.7];
+      const altitude = 0;
+
       const ve = new VektorEmbedded();
       await ve.init(true)
-      ve.mount(sessionToken, divId);
+      ve.mount(sessionToken, divId, {
+          enableContextMenu: true,
+          enableMarkup: true,
+          enableClippingPlanes: true,
+          enableMeasuring: true,
+          useLightMap: true,
+          drawModelsOnTopOfMap: true,
+          isElementSelectionLimitedToRooms: false,
+        },
+        // Path to room information
+        {
+            path: [], // use all props
+            roomIdentifier: ['Ifc-Name'],
+            roomNumber: ['Ifc-Name'],
+            extra: {
+                roomName: ['Ifc-Name'],
+            },
+        }
+      );
       // const vek = new VektorEmbeddedBasic(sessionToken, divId);
       // const vek = await VektorEmbeddedBasic.create(sessionToken, divId);
       // console.log("base =", vek);
+
+      const showMapLayer = (layerName) => {
+        ve.getWmsLayers('https://kartta.hel.fi/ws/geoserver/avoindata/wms').then((layers) => {
+          for (let item of layers) console.log(item.title);
+          const layer = layers.find((item) => item.title === layerName);
+          console.log("Found map layer", layer);
+          return ve.addMapLayer(layer.id);
+        });
+      };
+
+      await ve.setTerrain('FINLAND_10M');
       await ve.addModel(modelId, coordinateSystem)
+
+      console.log("Showing map layer");
+      // showMapLayer('Ortoilmakuva');
+
+
       ve.flyToModel(modelId);
 
-    }
+    },
   }
 }
 </script>
